490cedd903f21eda8e88be0ac130c69a0b2357ee
Parth Shah <parthshah7099@gmail.com>
2025-01-01 16:25:31 +0530

    1_LW

---
+++ LW_1.2_1.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("My name is Parth Shah.\n");
+	printf("I am 20 years old.\n");
+	printf("I was the student of Sanakar Bharti Vidhyalay school.\n");
+	getch();
+	return 0;
+}
---
+++ LW_1.2_4.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*     **        *\n");
+	printf("*    *  *      *\n");
+	printf("*   *    *    *\n");
+	printf("*  *      *  *\n");
+	printf("* *        **\n");
+	printf("*\nd");
+	getch();
+	return 0;
+}
